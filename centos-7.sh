7e4394199765841f082d1a8b3b8b6f9577ec2b97
Alex Feigin <alex.feigin@gmail.com>
2016-08-09 16:35:23 +0300

    add user devel to sudoers using group wheel

--- centos-7.sh
+++ centos-7.sh
@@ -33,6 +33,7 @@ develvncpwd="$develpwd"pass
 # add user devel and setup vnc
 useradd -c "User $develuser" $develuser
 echo $develpwd | passwd $develuser --stdin
+usermod -aG wheel $develuser
 sed -i "s:^#%wheel:UNCOMMENT%wheel:g" /etc/sudoers
 sed -i "s:^%wheel:#%wheel:g" /etc/sudoers
 sed -i "s:^UNCOMMENT%wheel:%wheel:g" /etc/sudoers
