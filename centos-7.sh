c1cd25ea5c10aa0c1caf1d2ccadd8cb4e6008830
Alex Feigin <alex.feigin@gmail.com>
2016-08-09 17:34:14 +0300

    sudoers patch fix

--- centos-7.sh
+++ centos-7.sh
@@ -34,8 +34,8 @@ develvncpwd="$develpwd"pass
 useradd -c "User $develuser" $develuser
 echo $develpwd | passwd $develuser --stdin
 usermod -aG wheel $develuser
-sed -i "s:^#%wheel:UNCOMMENT%wheel:g" /etc/sudoers
-sed -i "s:^%wheel:#%wheel:g" /etc/sudoers
+sed -i "s:^# %wheel:UNCOMMENT%wheel:g" /etc/sudoers
+sed -i "s:^%wheel:# %wheel:g" /etc/sudoers
 sed -i "s:^UNCOMMENT%wheel:%wheel:g" /etc/sudoers
 
 cp /lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@:1.service
