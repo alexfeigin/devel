7afd539f4813b0a567fd48f166d6d10cc9683e8c
Alex Feigin <alex.feigin@gmail.com>
2016-08-21 23:05:59 +0300

    some commands run quieter - some logging is done - new screenrc  unimgr is now cloned on spinup

--- centos-7.sh
+++ centos-7.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# install vnc server gnome and dev tools
-yum update -y
-yum install -y wget
 
-# Download and unpack maven jdk eclipse chrome - and yum install devel tools and desktop async
+echo Run yum update
+yum update -y > update.log 2>&1
+echo Install wget for easyier scripting
+yum install -y wget >> update.log 2>&1
+
+echo Download and unpack maven jdk eclipse chrome - and yum install devel tools and desktop async
 for part in desktop-tools-yum maven jdk eclipse chrome-rpm; do
 	wget -q https://rawgit.com/alexfeigin/devel/master/get-$part.sh
 	chmod +x ./get-$part.sh
@@ -17,7 +19,7 @@ develuser="devel"
 develpwd="devel"
 develvncpwd="develpass"
 
-# Add user devel and let sudoers run with no pass
+echo  Add user devel and let sudoers run with no pass
 useradd -c "User $develuser" $develuser
 echo $develpwd | passwd $develuser --stdin
 usermod -aG wheel $develuser
@@ -27,7 +29,7 @@ sed -i "s:^UNCOMMENT%wheel:%wheel:g" /etc/sudoers
 
 su $develuser -l -c logout
 
-# Create vnc server services for root :1 and for devel user :2
+echo Create vnc server services for root :1 and for devel user :2
 cp /lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@:1.service
 cp /lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@:2.service
 sed -i "s:<USER>:root:g" /etc/systemd/system/vncserver@:1.service
@@ -44,29 +46,33 @@ echo $develvncpwd | vncpasswd -f > /home/$develuser/.vnc/passwd; chown $develuse
 echo rootpass | vncpasswd -f > /root/.vnc/passwd
 chmod 600 /home/$develuser/.vnc/passwd /root/.vnc/passwd
 
-# Override screen PROMPT_COMMAND with hack for backtick display branch
+echo Override screen PROMPT_COMMAND with hack for backtick display branch
 echo "echo \$PWD > /tmp/currpwd" > /etc/sysconfig/bash-prompt-screen
 chmod +x /etc/sysconfig/bash-prompt-screen
 
-# Maven settings.xml for opendaylight devs
+echo Maven settings.xml for opendaylight devs
 runuser -l $develuser -c "mkdir -p /home/$develuser/.m2"
 wget -q -O - https://cdn.rawgit.com/opendaylight/odlparent/master/settings.xml > /home/$develuser/.m2/settings.xml
 
-# Add maven options - some might be deprecated
+echo Add maven options - some might be deprecated
 echo 'export MAVEN_OPTS='"'"'-Xmx1048m -XX:MaxPermSize=512m'"'" >> /home/devel/.bashrc
 
-# Download gitconfig for useful git aliases and setup
+echo Download gitconfig for useful git aliases and setup
 wget -q -O - https://cdn.rawgit.com/alexfeigin/devel/master/gitconfig > /etc/gitconfig
 
-# Disable firewalld and NetworkManager enable legacy network
-systemctl disable firewalld
-systemctl stop firewalld
-systemctl disable NetworkManager
-systemctl stop NetworkManager
-systemctl start network
-systemctl enable network
+echo Disable firewalld and NetworkManager enable legacy network
+netsvc()
+{
+	systemctl disable firewalld;
+	systemctl stop firewalld;
+	systemctl disable NetworkManager;
+	systemctl stop NetworkManager;
+	systemctl start network;
+	systemctl enable network;
+}
+netsvc > netsvc.log 2>&1
 
-# Setup terminal shortcut (create script, run as autostart app in gnome)
+echo "Setup terminal shortcut (create script, run as autostart app in gnome)"
 mkdir /usr/share/devel
 cat << EOF > /usr/share/devel/term-scut.sh
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
@@ -87,16 +93,16 @@ Type=Application
 X-GNOME-Autostart-enabled=true
 EOF
 
-# Fix "Authentication is required to set the network proxy used for downloading packages" message
+echo 'Fix "Authentication is required to set the network proxy used for downloading packages" message'
 echo "X-GNOME-Autostart-enabled=false" >> /etc/xdg/autostart/gnome-software-service.desktop
 
-# Create an identity file ssh-keygen
-runuser -l $develuser -c "mkdir ~/.ssh; cd ~/.ssh; ssh-keygen -f id_rsa -t rsa -N ''"
+echo Create an identity file ssh-keygen
+runuser -l $develuser -c "mkdir ~/.ssh; cd ~/.ssh; ssh-keygen -f id_rsa -t rsa -N ''" > id.log 2>&1
 
-# Create sources dir and clone unimgr
-su $develuser -c 'mkdir /home/$develuser/sources; cd /home/$develuser/sources; git clone https://git.opendaylight.org/gerrit/p/unimgr;'
+echo Create sources dir and clone unimgr
+su $develuser -c 'mkdir /home/$develuser/sources; cd /home/$develuser/sources; git clone https://git.opendaylight.org/gerrit/p/unimgr;' > clone.log 2>&1
 
-# Screenrc
+echo generate /home/$develuser/.screenrc
 cat << EOF > /tmp/screenrc
 hardstatus alwayslastline
 hardstatus alwayslastline "%-w%{.bw}%n %t%{-}%+w %-60= %1\`"
@@ -122,7 +128,7 @@ EOF
 su $develuser -c "cat /tmp/gcbsh > /home/$develuser/git-current-branch.sh"
 chmod +x /home/$develuser/git-current-branch.sh
 
-# Modify PS1 in bashrc
+echo Modify PS1 in .bashrc
 echo "export PS1='"'[\u@\[`[ $? = 0 ] && X=2 || X=1; tput setaf $X`\]\h\[`tput sgr0`\]:$PWD]\$ '"'" >> /home/$develuser/.bashrc
 
 # TODO: Make script quiet - only output to screen intresting info or progress
