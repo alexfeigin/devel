0f3fb80b6758515bb93d1e6dfea478eabb277ccb
Alex Feigin <alex.feigin@gmail.com>
2016-08-28 20:32:20 +0300

    network typo

--- centos-7.sh
+++ centos-7.sh
@@ -12,7 +12,7 @@ log "Install wget for easyier scripting"
 yum install -y wget >> $logdir/update.log 2>&1
 
 log "Install prerequisites async"
-for part in netwrok desktop-tools-yum maven eclipse chrome-rpm ; do
+for part in network desktop-tools-yum maven eclipse chrome-rpm ; do
 	log "Installing $part"
 	getpart $part > $logdir/$part.log 2>&1 &
 done
