e6d16e3217c7b485393e7ded58e52e4e4d50725c
Alex Feigin <alex.feigin@gmail.com>
2016-08-29 14:58:58 +0300

    git setup is now run with develuser

--- setup-git.sh
+++ setup-git.sh
@@ -2,8 +2,8 @@
 . utils.sh
 
 log "Set user git"
-git config --global user.email $gitemail
-git config --global user.name "$gituser"
+runuser -l $develuser -c "git config --global user.email $gitemail"
+runuser -l $develuser -c 'git config --global user.name "'"$gituser"'"'
 
 log "Download gitconfig for useful git aliases and setup"
 wget -q -O - https://cdn.rawgit.com/alexfeigin/devel/master/gitconfig > /etc/gitconfig
