3893531b7cd4ed3684522c566dbfa4cb4bc963f7
Ravit <ravit.peretz@hpe.com>
2016-08-29 14:46:52 +0300

    adding spaces support for git.user

--- setup-git.sh
+++ setup-git.sh
@@ -3,7 +3,7 @@
 
 log "Set user git"
 git config --global user.email $gitemail
-git config --global user.name $gituser
+git config --global user.name "$gituser"
 
 log "Download gitconfig for useful git aliases and setup"
 wget -q -O - https://cdn.rawgit.com/alexfeigin/devel/master/gitconfig > /etc/gitconfig
