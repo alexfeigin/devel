a1460b15dbd66b37a58a882a96bf534a420d6b51
Alex Feigin <alex.feigin@gmail.com>
2016-09-19 18:40:38 +0300

    network tools fix

--- get-desktop-tools-yum.sh
+++ get-desktop-tools-yum.sh
@@ -4,7 +4,7 @@ log installing groups '"Development Tools" "GNOME Desktop" "Graphical Administra
 yum groupinstall -y "Development Tools" "GNOME Desktop" "Graphical Administration Tools"
 
 log installing yum packages 
-yum -y install epel-release network-tools vim
+yum -y install epel-release net-tools vim
 yum -y \
 install python-{devel,setuptools,virtualenv} \
 @development \
