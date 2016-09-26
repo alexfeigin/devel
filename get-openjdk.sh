b636a407fc4bf3066c5d0779faab5cd3f0892a33
Alex Feigin <alex.feigin@gmail.com>
2016-09-26 17:48:28 +0300

    openjdk version update breaks install script - support next versions of java 1.8 openjdk

--- get-openjdk.sh
+++ get-openjdk.sh
@@ -7,7 +7,8 @@ else
 	log install java-1.8.0-openjdk-devel
 	yum install -y java-1.8.0-openjdk-devel
 
-	log creating link: ln -s /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.101-3.b13.el7_2.x86_64 /usr/local/java
+	lvjava=$(find /usr/lib/jvm/ -name 'java-1.8.0-openjdk-1.8.*.x86_64' | head -1)
+	log creating link: ln -s $lvjava /usr/local/java
 	ln -s /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.101-3.b13.el7_2.x86_64 /usr/local/java
 
 	log createing /etc/profile.d/java.sh
