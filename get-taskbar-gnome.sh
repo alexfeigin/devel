30254a7585b5563bee1452af2851d73250ba288f
Alex Feigin <alex.feigin@gmail.com>
2016-09-27 15:25:00 +0300

    missing quotation marks

--- get-taskbar-gnome.sh
+++ get-taskbar-gnome.sh
@@ -6,7 +6,7 @@ if [[ "X$(echo $prevexts | grep -Eo TaskBar)" == "X" ]]; then
 	wget -O /tmp/taskbar.zip "https://extensions.gnome.org/download-extension/taskbar@zpydr.shell-extension.zip?version_tag=5877"
 	mkdir -p /usr/share/gnome-shell/extensions/TaskBar@zpydr/
 	unzip /tmp/taskbar.zip -d /usr/share/gnome-shell/extensions/TaskBar@zpydr/
-	if [[ "X$prevexts" == "X" ]]; then ext=['TaskBar@zpydr']; else ext="[$prevexts, 'TaskBar@zpydr']"; fi
+	if [[ "X$prevexts" == "X" ]]; then ext="['TaskBar@zpydr']"; else ext="[$prevexts, 'TaskBar@zpydr']"; fi
 	gsettings set org.gnome.shell enabled-extensions "$ext"
 else
 	log "TaskBar@zpydr already installed"
