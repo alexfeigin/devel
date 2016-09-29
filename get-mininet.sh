6e26c9dacb3c0813b5bc59312078fb661e59f36b
Alex Feigin <alex.feigin@gmail.com>
2016-09-29 02:00:26 -0700

    fix using git as root with no username or email - git am fails

--- get-mininet.sh
+++ get-mininet.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 . utils.sh
 if [[ -e /root/sources/mininet ]]; then
-	log mininet already installed
+	log "mininet already installed"
 else
-	log make sure we have git
+	log "make sure we have git"
 	yum install -y git
-	log clone into mininet
+	gitconfigured=$(git config -l | grep "user.email")
+	if [[ "X$gitconfigured" == "X" ]]; then
+		if [[ "X$gituser" == "X" ]]; then 
+			gituser="demo"
+			gitemail="demo@mail.com"
+		fi 
+		git config --global user.email "$gituser"
+		git config --global user.name "$gitemail"
+	fi
+	log "clone into mininet"
 	mkdir -p /root/sources
 	cd /root/sources
 	git clone https://github.com/mininet/mininet.git
 	cd mininet
 	latest=$(git tag | grep -Ev '[ac-z]' | sort -nr | head -1)
-	log checkout $latest mininet and create branch for patched/$latest
+	log "checkout $latest mininet and create branch for patched/$latest"
 	git checkout $latest
 	git checkout -b patched/$latest
-	log apply mininet patch to enable install on CentOS
+	log "apply mininet patch to enable install on CentOS"
 	wget https://rawgit.com/alexfeigin/devel/master/mininet-centos.patch
 	git am -3 < mininet-centos.patch
-	log start ./util/install.sh -nf
+	log "start ./util/install.sh -nf"
 	./util/install.sh -nf
 	wget https://rawgit.com/alexfeigin/devel/master/topo-custom.py -O sources/mininet/custom/topo-custom.py
-	log Finished install mininet - try mn --test pingall
+	log "Finished install mininet - try mn --test pingall"
 fi
