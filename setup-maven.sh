e24273cc7594d26043750453e31966ebe191f75c
Alex Feigin <alex.feigin@gmail.com>
2016-09-29 22:06:53 +0300

    proxies fix in maven setup

--- setup-maven.sh
+++ setup-maven.sh
@@ -4,6 +4,7 @@
 log "Maven settings.xml for opendaylight devs"
 runuser -l $develuser -c "mkdir -p /home/$develuser/.m2"
 getproxy mvnsets "https://cdn.rawgit.com/opendaylight/odlparent/master/settings.xml"
+. .proxies.sh
 wget -q -O - $mvnsetsproxy > /home/$develuser/.m2/settings.xml
 chown $develuser /home/$develuser/.m2/settings.xml
 chgrp $develuser /home/$develuser/.m2/settings.xml
