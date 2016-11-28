dccc0f80ec3323a2914dfb57fead7a06503aae14
Alex Feigin <alex.feigin@gmail.com>
2016-11-28 14:27:01 +0200

    sudo removed from openvswitch utils

--- get-openvswitch.sh
+++ get-openvswitch.sh
@@ -16,7 +16,7 @@ else
 	_f()
 	{
 		if [[ \${COMP_CWORD} -gt 1 ]]; then COMPREPLY=(); return 0; exit; fi
-		opts=\$(sudo ovs-vsctl list-br 2>/dev/null)
+		opts=\$(ovs-vsctl list-br 2>/dev/null)
 		cur="\${COMP_WORDS[COMP_CWORD]}"
 		COMPREPLY=( \$(compgen -W "\${opts}" -- \${cur}) )
 		return 0
@@ -28,7 +28,7 @@ else
 	{
 		local br=\$1
 		if [[ "\$br" == "" ]]; then echo "usage getdpid <bridge-name>"; return; fi
-		local hexdpid=\$(sudo ovs-vsctl -- get bridge \$br datapath-id 2>/dev/null)
+		local hexdpid=\$(ovs-vsctl -- get bridge \$br datapath-id 2>/dev/null)
 		if [[ "\$hexdpid" == "" ]]; then echo no bridge named \$br; return; fi
 		python -c "print(int(\$hexdpid,16))"
 	}
@@ -36,7 +36,7 @@ else
 	{
 		local br=\$1
 		if [[ "\$br" == "" ]]; then echo "usage dumpflows <bridge-name>"; return; fi
-		local hexdpid=\$(sudo ovs-vsctl -- get bridge \$br datapath-id 2>/dev/null)
+		local hexdpid=\$(ovs-vsctl -- get bridge \$br datapath-id 2>/dev/null)
 		if [[ "\$hexdpid" == "" ]]; then echo no bridge named \$br; return; fi
 		ovs-ofctl dump-flows \$br -OOpenflow13
 	}
@@ -44,7 +44,7 @@ else
 	{
 		local br=\$1
 		if [[ "\$br" == "" ]]; then echo "usage dumpgroups <bridge-name>"; return; fi
-		local hexdpid=\$(sudo ovs-vsctl -- get bridge \$br datapath-id 2>/dev/null)
+		local hexdpid=\$(ovs-vsctl -- get bridge \$br datapath-id 2>/dev/null)
 		if [[ "\$hexdpid" == "" ]]; then echo no bridge named \$br; return; fi
 		ovs-ofctl dump-groups \$br -OOpenflow13
 	}
