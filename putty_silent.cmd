b8a6c8e4a604e569bdaa9958e865dcb779a1d66f
Alex Feigin <alex.feigin@gmail.com>
2017-01-24 15:00:11 +0200

    putty silent install

---
+++ putty_silent.cmd
@@ -0,0 +1,43 @@
+@echo off
+CLS
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+::::::::::::::::::::::::::::
+::START
+::::::::::::::::::::::::::::
+REM Run shell as admin (example) - put here code as you like
+echo $client = new-object System.Net.WebClient > download.ps1
+echo $client.DownloadFile("https://the.earth.li/~sgtatham/putty/0.67/x86/putty-0.67-installer.exe","C:\Users\contextream\Desktop\client\putty-0.67-installer.exe") >> download.ps1
+powershell -ExecutionPolicy Unrestricted -File download.ps1
+del download.ps1
+C:\Users\contextream\Desktop\client\putty-0.67-installer.exe /VERYSILENT
+del C:\Users\contextream\Desktop\client\putty-0.67-installer.exe 
+setx path "%path%;c:\Program Files (x86)\PuTTY"
+pause
+
