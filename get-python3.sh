0c3b6189edb9fca0a9f3b680644bc93d463cb009
Alex Feigin <alex.feigin@gmail.com>
2017-11-13 11:31:27 +0200

    get-python3.sh

---
+++ get-python3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# install python34 and pip install some useful modules
+. utils.sh
+yum install -y yum-tools
+yum-builddep python
+yum install -y python34*
+
+pip3 install --upgrade pip
+pip3 install bs4
+pip3 install lxml
+
