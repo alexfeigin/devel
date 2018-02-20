0082a80925da243040481d35c7d68229f6947cb7
Alex Feigin <alex.feigin@gmail.com>
2018-02-20 13:24:02 +0200

    get-eclipse.sh updated to eclipse oxygen 2 version

--- get-eclipse.sh
+++ get-eclipse.sh
@@ -2,43 +2,53 @@
 # setup eclipse
 . utils.sh
 
-
-if [[ -e /opt/eclipse-neon ]]; then 
+eclipsever="eclipse-oxygen"
+eclipsename="Eclipse Oxygen"
+if [[ -e /opt/$eclipsever ]]; then 
 	log "Eclipse already installed"
 else
-	getproxy eclipse "http://mirror.switch.ch/eclipse/technology/epp/downloads/release/neon/R/eclipse-java-neon-R-linux-gtk-x86_64.tar.gz"
+	tarname="eclipse-java-oxygen-2-linux-gtk-x86_64.tar.gz"
+	getproxy eclipse "http://mirror.switch.ch/eclipse/technology/epp/downloads/release/oxygen/2/eclipse-java-oxygen-2-linux-gtk-x86_64.tar.gz"
 	. .proxies.sh
 	pushd /opt
-	log Downloading neon eclipse from mirror $eclipseproxy
-	wget $eclipseproxy -O eclipse-java-neon-R-linux-gtk-x86_64.tar.gz
-	log Untaring eclipse
-	tar xzf eclipse-java-neon-R-linux-gtk-x86_64.tar.gz
-	mv eclipse eclipse-neon 
-	log Creating link: ln -s /opt/eclipse-neon /usr/local/eclipse
-	ln -s /opt/eclipse-neon /usr/local/eclipse
-	log cleaning tar file
-	rm -f eclipse-java-neon-R-linux-gtk-x86_64.tar.gz
-	popd
-	log Creating /etc/profile.d/eclipse.sh
-	cat <<-EOF > /etc/profile.d/eclipse.sh
-	export ECLIPSE_HOME=/usr/local/eclipse
-	export PATH=\${ECLIPSE_HOME}:\${PATH}
-	EOF
-	chmod +x /etc/profile.d/eclipse.sh
+	log "Downloading $eclipsever from mirror $eclipseproxy"
+	wget $eclipseproxy -O $tarname
+	log "Untaring eclipse"
+	tar xzf $tarname
+	mv eclipse $eclipsever
+	log "Cleaning tar file"
+	rm -f $tarname
 	log "Extending eclipse memory"
-	sed -i "s/-Xmx1024m/-Xmx2048m/g" /opt/eclipse-neon/eclipse.ini
+	sed -i "s/-Xmx.*m/-Xmx2048m/g" /opt/$eclipsever/eclipse.ini
+	if [[ -e /usr/local/eclipse ]]; then
+		log "/usr/local/eclipse found, you may want to unlink your local eclipse and link to $eclipsename version"
+	else
+		log "Creating link: ln -s /opt/$eclipsever /usr/local/eclipse"
+		ln -s /opt/$eclipsever /usr/local/eclipse
+	fi
+	popd
+	if [[ -e /etc/profile.d/eclipse.sh ]]; then
+		log "/etc/profile.d/eclipse.sh exists"
+	else
+		log "Creating /etc/profile.d/eclipse.sh"
+		cat <<-EOF > /etc/profile.d/eclipse.sh
+		export ECLIPSE_HOME=/usr/local/eclipse
+		export PATH=\${ECLIPSE_HOME}:\${PATH}
+		EOF
+		chmod +x /etc/profile.d/eclipse.sh
+	fi
 	log "Adding eclipse to menu"
-	cat <<-EOF > /usr/share/applications/eclipse.desktop
+	cat <<-EOF > /usr/share/applications/"$eclipsever".desktop
 	[Desktop Entry]
 	Version=1.0
-	Comment=Eclipse Neon
-	Name=Eclipse Neon
-	Icon=/opt/eclipse-neon/icon.xpm
-	Exec=/opt/eclipse-neon/eclipse
+	Comment=$eclipsename
+	Name=$eclipsename
+	Icon=/opt/$eclipsever/icon.xpm
+	Exec=/opt/$eclipsever/eclipse
 	Terminal=false
 	Type=Application
 	Categories=GNOME;Development;IDE;Java;Programming;
 	EOF
-	chmod +x /usr/share/applications/eclipse.desktop
+	chmod +x /usr/share/applications/"$eclipsever".desktop
 	log Finished
 fi 
